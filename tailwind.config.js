eb0f905bfc95152798f2f1e027036394db3262d6
Renato Gonzales <rent.gonz@gmail.com>
2022-05-19 21:43:56 -0500

    Se agrego el main page
    Se agrego algunos elementos html y configuracion personalizada a tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,19 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'bg-uphold-light': '#eef0f5'
+      },
+      fontFamily: {
+        'sans': ['Proxima Nova', ...defaultTheme.fontFamily.sans]
+      },
+      fontSize: {
+        'tiny': '0.8rem'
+      }
+    },
   },
   plugins: [],
 }
